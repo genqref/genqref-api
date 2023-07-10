ced0ec0db2a1e34b61c8e6f3e2dd71ae98426395
Phil Hofmann <phil@200ok.ch>
2023-07-10 22:23:34 +0200

    initial commit

---
+++ src/genqref_api/main.clj
@@ -0,0 +1,13 @@
+(ns genqref-api.main
+  (:gen-class)
+  (:require [duct.core :as duct]))
+
+(duct/load-hierarchy)
+
+(defn -main [& args]
+  (let [keys     (or (duct/parse-keys args) [:duct/daemon])
+        profiles [:duct.profile/prod]]
+    (-> (duct/resource "genqref_api/config.edn")
+        (duct/read-config)
+        (duct/exec-config profiles keys))
+    (System/exit 0)))
